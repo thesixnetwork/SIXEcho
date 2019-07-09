33f858123352fdf181c4d0e86417b763a488490f
Theerat Tassanai <theerat@Theerats-MacBook-Pro.local>
2019-07-09 16:21:02 +0700

    let function upload epub file to server

--- sixecho/client.py
+++ sixecho/client.py
@@ -14,6 +14,8 @@ from concurrent.futures import ThreadPoolExecutor
 import requests
 from datasketch import MinHash
 from pythainlp import word_tokenize
+from epub_conversion.utils import open_book
+import epub_conversion as ec
 
 
 def mygrouper(n, iterable):
@@ -126,15 +128,25 @@ class Client(object):
         """
         return self.min_hash.digest()
 
-    def generate(self, str=None, fpath=None):
+    def generate(self, str=None, txtpath=None, epubpath=None):
         """Generate minhash with new value from string or file
         we use minhash from https://ekzhu.github.io/datasketch/_modules/datasketch/minhash.html#MinHash.update
         Args:
             str(string)     - Optional  :   string whose minhash to be computed.
-            fpath(string)   - Optional  :   path file to be computed.
+            txtpath(string)   - Optional  :   path of text file to be computed.
+            epubpath(string)   - Optional  :   path of epub file to be computed.
         """
-        if fpath:
-            self.load_file(fpath)
+        if txtpath:
+            self.load_file(txtpath)
+        elif epubpath:
+            size = len(epubpath.split('.'))
+            name = epubpath.split('.')[size - 2]
+            size = len(name.split('/'))
+            name = name.split('/')[size-1]
+            name = name.replace("/","")
+            name = name+'.txt'
+            self.write2text(self.readepub(epubpath), name)
+            self.load_file('./books/'+name)
         else:
             sha256 = hashlib.sha256()
             sha256.update(str.encode())
@@ -221,3 +233,42 @@ class Client(object):
                                  length=50)
         self.sha256 = sha256.hexdigest()
         self.fileSize = fileSize
+
+    def readepub(self, fpath):
+        list_text = []
+        book = open_book(fpath)
+        lines = ec.utils.convert_epub_to_lines(book)
+        for line in lines:
+
+            text = ec.utils.convert_lines_to_text(str(line), "test")
+            text = list(text)
+            for ele in text:
+                list_text.append(ele)
+        return list_text
+
+    def write2text(self, list_text, opname):
+        fpath = './books/'+opname
+        file = open(fpath, 'w')
+        for ele in list_text:
+            file.write(ele)
+
+meta_books = {
+    "category_id": "1",
+    "publisher_id": "1",
+    "title": "nonnon",
+    "author": "theeratnon",
+    "country_of_origin": "THA",
+    "language": "th",
+    "paperback": "134",
+    "publish_date": 1560869555
+}
+client = Client(
+    host_url=
+    "https://mc64byvj0i.execute-api.ap-southeast-1.amazonaws.com/prod/",
+    api_key="4S8Vps2d7t3FiYgt07lQL1i620JRR8Ena0DWhVmv",
+    max_workers=2,
+    meta_books=meta_books)
+
+client.generate(epubpath="./books/book.epub")
+client.digest()
+client.upload()
