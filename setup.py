e84fa6f23512247b0cf9d9f8749439e0d9adcfb3
Prach Hormanee <prach.hormanee@gmail.com>
2019-07-31 16:05:43 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 REQUIRED_PACKAGES = [
-    'datasketch>=1.4.3,<1.5.0', 'requests==2.20.1', 'pythainlp==2.0.6'
+    'datasketch>=1.4.3,<1.5.0', 'requests==2.20.1', 'pythainlp==2.0.6', 'epub_conversion==1.0.7'
 ]
 
 setuptools.setup(
